a820175da7417118c372c1d78b806d4b450838d1
mabrady444 <91609183+mabrady444@users.noreply.github.com>
2021-10-13 16:36:42 -0400

    state machine test

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Chassis.h"
 #include "Rangefinder.h"
 
+
 const uint8_t IR_DETECTOR_PIN = 15; // define the pin for the IR receiver
 
 //global variables and constants
@@ -49,12 +50,12 @@ enum ROBOT_STATE {
     ArriveRoof,     //arrive at a roof 
     ReturnRoof,     //return to the roof with the new panel
     LeaveRoof,      //leave for the platform deposit
-    DistanceTest    //test the ultrasonic distance function
+    
     
 
 };
 
-ROBOT_STATE robotState = DistanceTest;    //innitialize the state machine 
+ROBOT_STATE robotState = ArriveRoof;    //innitialize the state machine 
 
 //--------------------------------------------------------------------------------------------------------------------------------
 const int STAGING_POS = 0;              //constants for positions of the arm
@@ -82,64 +83,80 @@ void setup() {
 int effort = 0;
 int setpoint = 0;
 float distance = 0;
+float oldDistance=0;
 
 bool manualMode = false;
 
+float getDistance(){           //correct ultrasonic distance
+oldDistance=distance;
+distance=ultrasonic.getDistanceCM();
+if(oldDistance==0){
+    oldDistance=distance;
+}
+if(abs(distance-oldDistance)>=4){
+distance=oldDistance;
+Serial.println("filtered bad reading");
+}
+return distance;
+}
+
+
+
 void loop()
 {
-    distance = ultrasonic.getDistanceCM(); //get the distance from the untrasoonic
-    Serial.println(distance);
+    //distance = ultrasonic.getDistanceCM(); //get the distance from the untrasoonic
+    Serial.println(getDistance());
 
-    int key = decoder.getKeyCode();
-    //Serial.println(key);
-    if (key == KEY_VOL_PLUS)
-    { //servo?
-        effort++;
-    }
-    else if (key == KEY_STOP){ //
-        effort += 10;
-    }
-    else if (key == KEY_RIGHT) {       //blue motor arm down
-        effort += 100;
-    }
-    else if (key == KEY_VOL_MINUS){
-        effort--;
-    }
-    else if (key == KEY_SETUP){
-        effort -= 10;
-        //key == KEY_LEFT)
-    }
-    else if (key == KEY_LEFT){          //blue motor arm up
-        effort -= 100;
-        //Serial.println(effort);
-    }
-    else if (key == KEY_PLAY){         //preset staging platform button and position
-        setpoint = STAGING_POS;
-    }
-    else if (key == KEY_UP){           //preset 25 degree roof button and position for pickup
-        setpoint = ROOF_25_PICKUP;
-    }
-    else if (key == KEY_ENTER){         //preset 25 degreee roof button and position for placement
-        setpoint = ROOF_25_PLACE;
-    }
-    else if (key == KEY_SEVEN){        //perset servo gripper open
-        gripper.write(GRIPPER_OPEN);
-    }
-    else if (key == KEY_NINE){         //preset gripper closed
-        gripper.write(GRIPPER_CLOSED);
-    } else if (key == KEY_FIVE) {
-        manualMode = !manualMode;
+    // int key = decoder.getKeyCode();
+    // //Serial.println(key);
+    // if (key == KEY_VOL_PLUS)
+    // { //servo?
+    //     effort++;
+    // }
+    // else if (key == KEY_STOP){ //
+    //     effort += 10;
+    // }
+    // else if (key == KEY_RIGHT) {       //blue motor arm down
+    //     effort += 100;
+    // }
+    // else if (key == KEY_VOL_MINUS){
+    //     effort--;
+    // }
+    // else if (key == KEY_SETUP){
+    //     effort -= 10;
+    //     //key == KEY_LEFT)
+    // }
+    // else if (key == KEY_LEFT){          //blue motor arm up
+    //     effort -= 100;
+    //     //Serial.println(effort);
+    // }
+    // else if (key == KEY_PLAY){         //preset staging platform button and position
+    //     setpoint = STAGING_POS;
+    // }
+    // else if (key == KEY_UP){           //preset 25 degree roof button and position for pickup
+    //     setpoint = ROOF_25_PICKUP;
+    // }
+    // else if (key == KEY_ENTER){         //preset 25 degreee roof button and position for placement
+    //     setpoint = ROOF_25_PLACE;
+    // }
+    // else if (key == KEY_SEVEN){        //perset servo gripper open
+    //     gripper.write(GRIPPER_OPEN);
+    // }
+    // else if (key == KEY_NINE){         //preset gripper closed
+    //     gripper.write(GRIPPER_CLOSED);
+    // } else if (key == KEY_FIVE) {
+    //     manualMode = !manualMode;
         
-        Serial.printf("ManualMode: %d\n", manualMode);
-    }
+    //     Serial.printf("ManualMode: %d\n", manualMode);
+    // }
 
-    effort = constrain(effort, -255, 255);
+    // effort = constrain(effort, -255, 255);
 
-    //(not both at the same time)
-    if (manualMode)
-        blueMotor.setEffortCorrected(effort); //use the corrected effort in order to place the arm manually
-    else
-        blueMotor.setPosition(setpoint); //use the perdetermined locations to position the arm
+    // //(not both at the same time)
+    // if (manualMode)
+    //     blueMotor.setEffortCorrected(effort); //use the corrected effort in order to place the arm manually
+    // else
+    //     blueMotor.setPosition(setpoint); //use the perdetermined locations to position the arm
 
     // Serial.printf("ultra dist: %.1f | %d | %ld\n", distance, effort, blueMotor.getPosition());
 
@@ -147,21 +164,26 @@ void loop()
 
     // int key = decoder.getKeyCode();
 //---------------------------------------------------------------------------------------------------------------------------------
+    
     switch (robotState)        //state of the robot state machine in main()
     {
 
         case ArriveRoof: //arrive following the line at a roof
-            if (ultrasonic.getDistanceCM() <= 30){
+            if (getDistance() <= 15){
+                setpoint = ROOF_25_PLACE;
+
                 robotState = FirstVisit; //set to the first position at a certain distance
             }
             else{
-                chassis.followPath(false); //follow the line otherwise, if there is an intersection, turn left
-            }  
+                //chassis.followPath(false); //follow the line otherwise, if there is an intersection, turn left
+            } 
             break;
         
         case FirstVisit:
+            Serial.println("next state");
+            delay(100000);
             gripper.write(GRIPPER_OPEN);            //set the gripper to open
-            setpoint = ROOF_25_PLACE;
+            
 
             delay(10);
             //adjust distance line  for best pickup drive foward
@@ -181,7 +203,7 @@ void loop()
             //follow line
             //turn (right?) at intersection
             //follow line again
-            if (ultrasonic.getDistanceCM() <= 30)
+            if (ultrasonic.getDistanceCM() <= 15)
             { 
                 robotState = SecondVisit;   //go to the staging platform state
             }
@@ -204,7 +226,7 @@ void loop()
             break;
 
     case ReturnRoof:                                   //return to the roof for the deposit of the new panel
-    if (ultrasonic.getDistanceCM() <= 30)
+    if (ultrasonic.getDistanceCM() <= 15)
         { 
             robotState = FirstVisit;   //go to the staging platform state
         }
@@ -213,10 +235,7 @@ void loop()
         }
         break;
 
-    case DistanceTest:
-    ultrasonic.getDistanceCM();
-    Serial.println("im finding the distance");
-    break;
+    
 
 
 
@@ -226,3 +245,5 @@ void loop()
             
         }
 }
+
+
